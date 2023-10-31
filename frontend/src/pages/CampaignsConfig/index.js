71775b741186a448a3276b224214f7e4c3482b80
Mateus <mateus_duarte20@outlook.com>
2023-10-31 20:57:05 -0300

    feat: change seconds and images

--- frontend/src/pages/CampaignsConfig/index.js
+++ frontend/src/pages/CampaignsConfig/index.js
@@ -153,10 +153,9 @@ const CampaignsConfig = () => {
                   onChange={e => handleOnChangeSettings(e)}
                 >
                   <MenuItem value={0}>Sem Intervalo</MenuItem>
-                  <MenuItem value={5}>5 segundos</MenuItem>
                   <MenuItem value={10}>10 segundos</MenuItem>
-                  <MenuItem value={15}>15 segundos</MenuItem>
                   <MenuItem value={20}>20 segundos</MenuItem>
+                  <MenuItem value={120}>120 segundos</MenuItem>
                 </Select>
               </FormControl>
             </Grid>
@@ -180,8 +179,8 @@ const CampaignsConfig = () => {
                   <MenuItem value={0}>Não definido</MenuItem>
                   <MenuItem value={5}>5 mensagens</MenuItem>
                   <MenuItem value={10}>10 mensagens</MenuItem>
-                  <MenuItem value={15}>15 mensagens</MenuItem>
                   <MenuItem value={20}>20 mensagens</MenuItem>
+                  <MenuItem value={60}>60 mensagens</MenuItem>
                 </Select>
               </FormControl>
             </Grid>
@@ -204,10 +203,10 @@ const CampaignsConfig = () => {
                 >
                   <MenuItem value={0}>Sem Intervalo</MenuItem>
                   <MenuItem value={20}>20 segundos</MenuItem>
-                  <MenuItem value={30}>30 segundos</MenuItem>
                   <MenuItem value={40}>40 segundos</MenuItem>
-                  <MenuItem value={50}>50 segundos</MenuItem>
                   <MenuItem value={60}>60 segundos</MenuItem>
+                  <MenuItem value={120}>120 segundos</MenuItem>
+                  <MenuItem value={180}>180 segundos</MenuItem>
                 </Select>
               </FormControl>
             </Grid>
